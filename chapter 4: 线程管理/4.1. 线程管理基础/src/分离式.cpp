cd326385f7bae335bcd0951f38c37fd821ff4bdf
pzh <1972508489@qq.com>
2019-08-30 16:31:47 +0800

    Update 分离式.cpp

--- chapter 4: 线程管理/4.1. 线程管理基础/src/分离式.cpp
+++ chapter 4: 线程管理/4.1. 线程管理基础/src/分离式.cpp
@@ -14,7 +14,7 @@ void func()
 int main(void)
 {
 	thread corr(func);   // 初始化线程并执行
-	corr.detach();       // 设定线程结束模式，采用分离式，线程将一直独立执行下去，直到主线程终止
+	corr.detach();       // 设定父子线程关系模式，采用分离式，线程将一直独立执行下去，直到主线程终止
 	
 	for(int i=0;i<100;i++)
 	{
