5a2f33bf895a5ee4b430a3df34535d6d898d9985
pzh <1972508489@qq.com>
2019-08-30 16:23:42 +0800

    Update Hello World.cpp

--- chapter 3: 入门和Hello, World/src/Hello World.cpp
+++ chapter 3: 入门和Hello, World/src/Hello World.cpp
@@ -18,5 +18,4 @@ int main(void)
 {
 	thread corr(thread_callback);    // 使用执行函数初始化和启动线程对象 
 	cout<<"Hello world from main thread."<<endl;
-	corr.join();    // 调用join方法，决定结束线程对象的方式
-}
+	corr.join();    // 调用join方法，设定父子线程关系模式，从而正常结束线程
