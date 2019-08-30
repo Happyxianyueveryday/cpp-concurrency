cd6b6df398af54fe213238df86a9201c167f911d
pzh <1972508489@qq.com>
2019-08-30 10:36:46 +0800

    Create Hello World.cpp

---
+++ chapter 3: 入门和Hello, World/src/Hello World.cpp
@@ -0,0 +1,22 @@
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * thread_callback: 线程对象的执行函数 
+ */ 
+void thread_callback()
+{
+	for(int i=0;i<3;i++)
+	{
+		cout<<"Hello world from concurrent thread."<<endl;
+	}
+}
+
+int main(void)
+{
+	thread corr(thread_callback);    // 使用执行函数初始化线程对象 
+	cout<<"Hello world from main thread."<<endl;
+	corr.join();    // 调用join方法，执行线程对象
+}
